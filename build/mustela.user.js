3cf30ab86ef61ee17bf51df681701ae02b1ce9af
haxibami <me@haxibami.net>
2025-09-10 23:27:18 +0900

    0.1.7

--- build/mustela.user.js
+++ build/mustela.user.js
@@ -1,6 +1,5 @@
 // ==UserScript==
-// @name        Mustela
-// @encoding    utf-8
+// @name        mustela
 // @namespace   https://github.com/haxibami/mustela
 // @homepageURL https://github.com/haxibami/mustela
 // @supportURL  https://github.com/haxibami/mustela/issues
@@ -8,11 +7,18 @@
 // @downloadURL https://github.com/haxibami/mustela/raw/main/build/mustela.user.js
 // @license     MIT
 // @match       *://*/*
-// @grant       unsafeWindow
-// @version     0.1.5
+// @grant       GM.addStyle
+// @version     0.1.7
 // @author      haxibami
 // @description Anti-anti-adblock user script for personal use.
 // @run-at      document-start
 // ==/UserScript==
 
-(()=>{var r=typeof unsafeWindow<"u"?unsafeWindow:window,p=r.RegExp.prototype.test,f=[/pagead2\.googlesyndication\.com/,/securepubads\.g\.doubleclick\.net/,/html-load\.com/,/content-loader\.com/];console.log("Ad scripts blocker active");r.XMLHttpRequest=new Proxy(r.XMLHttpRequest,{construct(n,e){let t=new n(...e),s={blockUrl:null};return new Proxy(t,{get(c,o,d){if(o==="open")return(y,i,...w)=>{if(f.some((b)=>p.call(b,i)))s.blockUrl=i,i="https://nobody.invalid";return c[o].apply(c,[y,i,...w])};if(o==="responseURL")return s.blockUrl||c[o];let l=c[o];return typeof l!=="function"||o==="onreadystatechange"?l:l.bind(c)},set(c,o,d){return c[o]=d,!0}})}});r.Element.prototype.setAttribute=new Proxy(r.Element.prototype.setAttribute,{apply(n,e,t){if(t[0]==="src"&&f.some((s)=>p.call(s,t[1])))t[1]=`data:text/javascript,${encodeURIComponent("(() => { 'use strict'; })();")}`;return Reflect.apply(n,e,t)}});var m=(n)=>{let e=0;for(let t=0,s=n.length;t<s;t++)e=(e<<5)-e+n.charCodeAt(t),e|=0;return e},u=Date.now(),a=u-u%86400000,R=[a,a-86400000,a+86400000];for(let n of R)r[`as_${m(`loader-check_${n}`)}`]=!0;})();
+(()=>{GM.addStyle(`
+    div[data-lyricid] { 
+        user-select: text;
+    }
+    #tpModal {
+        display: none !important;
+    }
+`);})();
--- src/index.ts
+++ src/index.ts
@@ -1,95 +1,12 @@
-type unsafeWindow = typeof window;
-declare const unsafeWindow: unsafeWindow;
-const Win = typeof unsafeWindow < "u" ? unsafeWindow : window;
-
-const OriginalRegExpTest = Win.RegExp.prototype.test;
-
-const blockDomains = [
-  /pagead2\.googlesyndication\.com/,
-  /securepubads\.g\.doubleclick\.net/,
-  /html-load\.com/,
-  /content-loader\.com/,
-];
-
-const invalidURL = "https://nobody.invalid";
-const noopScript = "(() => { 'use strict'; })();";
-
-console.log("Ad scripts blocker active");
-
-Win.XMLHttpRequest = new Proxy(Win.XMLHttpRequest, {
-  construct(target, args: []) {
-    const xhrInstance = new target(...args);
-
-    const state: { blockUrl: string | null } = {
-      blockUrl: null,
-    };
-
-    return new Proxy(xhrInstance, {
-      get(target, prop, _receiver) {
-        if (prop === "open") {
-          // @ts-expect-error
-          return (method: string, url: string, ...rest) => {
-            if (
-              blockDomains.some((pattern) =>
-                OriginalRegExpTest.call(pattern, url),
-              )
-            ) {
-              state.blockUrl = url;
-              url = invalidURL;
-            }
-            // @ts-expect-error
-            return target[prop].apply(target, [method, url, ...rest]);
-          };
-        }
-        if (prop === "responseURL") {
-          return state.blockUrl || target[prop];
-        }
-        // @ts-expect-error
-        const value = target[prop];
-        return typeof value !== "function" || prop === "onreadystatechange"
-          ? value
-          : value.bind(target);
-      },
-      set(target, prop, value) {
-        // @ts-expect-error
-        target[prop] = value;
-        return true;
-      },
-    });
-  },
-});
-
-Win.Element.prototype.setAttribute = new Proxy(
-  Win.Element.prototype.setAttribute,
-  {
-    apply(target, thisArg, args) {
-      if (
-        args[0] === "src" &&
-        blockDomains.some((pattern) =>
-          OriginalRegExpTest.call(pattern, args[1]),
-        )
-      ) {
-        args[1] = `data:text/javascript,${encodeURIComponent(noopScript)}`;
-      }
-      return Reflect.apply(target, thisArg, args);
-    },
-  },
-);
-
-const o = (n: string) => {
-  let e = 0;
-  for (let t = 0, r = n.length; t < r; t++) {
-    e = (e << 5) - e + n.charCodeAt(t);
-    e |= 0;
-  }
-  return e;
-};
-
-const a = Date.now();
-const s = a - (a % 86400000);
-const dates = [s, s - 86400000, s + 86400000];
-
-for (const date of dates) {
-  // @ts-expect-error
-  Win[`as_${o(`loader-check_${date}`)}`] = true;
+declare namespace GM {
+  function addStyle(css: string): HTMLElement;
 }
+
+GM.addStyle(`
+    div[data-lyricid] { 
+        user-select: text;
+    }
+    #tpModal {
+        display: none !important;
+    }
+`);
